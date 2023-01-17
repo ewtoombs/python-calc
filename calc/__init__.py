6fa66f010829afe96593993d900802e2e31b90af
Eric Toombs <not@public.net>
2023-01-17 00:26:12 -0500

    python has finally allowed for ISO dates without the ridiculous T, so I can use fromisoformat now.

--- calc/__init__.py
+++ calc/__init__.py
@@ -201,9 +201,11 @@ def norm_cdf(x):
 
 def iso2unix(s):
     from datetime import datetime, timezone
-    return datetime.strptime(s, '%Y-%m-%d %H:%M:%S.%f') \
-            .replace(tzinfo=timezone.utc) \
+    return (
+        datetime.fromisoformat(s)
+            .replace(tzinfo=timezone.utc)
             .timestamp()
+    )
 
 
 def unix2iso(t):
